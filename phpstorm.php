e21f3e28181cc24339dc4e15305ce3d3ad0b29ca
Natan Felles <natanfelles@gmail.com>
2016-03-21 01:55:52 -0300

    Add property to load function from Ispconfig class
    ### ISPConfig 3 Remote API to CodeIgniter
    https://github.com/natanfelles/codeigniter-ispconfig

--- phpstorm.php
+++ phpstorm.php
@@ -65,6 +65,7 @@ die('This file is used for development purposes only.');
  * @property CI_Xmlrpcs          $xmlrpcs                             XML-RPC server class
  * @property CI_Zip              $zip                                 Zip Compression Class
  * @property CI_Utf8             $utf8                                Provides support for UTF-8 environments
+ * @property Ispconfig           $ispconfig                           This class enables you to use the ISPConfig 3 Remote API
  */
 class CI_Controller {
 
@@ -119,6 +120,7 @@ class CI_Controller {
  * @property CI_Xmlrpcs          $xmlrpcs                             XML-RPC server class
  * @property CI_Zip              $zip                                 Zip Compression Class
  * @property CI_Utf8             $utf8                                Provides support for UTF-8 environments
+ * @property Ispconfig           $ispconfig                           This class enables you to use the ISPConfig 3 Remote API
  */
 class CI_Model {
 
